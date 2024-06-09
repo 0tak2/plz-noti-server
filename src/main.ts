cb352931a4604555df74f466f7f6a192c1dc6a0b
0tak2 <0tak2.code@gmail.com>
2024-06-09 23:08:46 +0900

    feat: CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
+
+initializeApp({
+  credential: applicationDefault(),
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
